e1b6d1223d63addaca4ed85e03b08b24e3ce5fa1
Taras Yemtsov <flamytwista@gmail.com>
2021-02-06 04:10:55 +0300

    Применил контейнер.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <p>hello</p>
+  <div class="n-container">
+    <p>
+      <span v-for="n in 30" :key="n">
+        1hello
+      </span>
+
+    </p>
+    <p v-for="n in 12" :key="n">
+      : {{ n }}
+    </p>
+
   </div>
 </template>
 
